823066e29455f70dc0da1b5e6920460eebe8e1ac
Kipngetich <khalifngeno@gmail.com>
2021-11-22 09:40:58 +0300

    add a new models plus migration

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    user_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=12)
+    bio = models.TextField()
+    profile_pic = models.FilePathField(path="/img")
--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -3,5 +3,6 @@ from accounts import views
 
 urlpatterns = [
     path('', views.account_home, name='account_home'),
+    path('accounts_index', views.accounts_index, name='accounts_index'),
 ]
 
